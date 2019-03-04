51b4f0c25c3adf236ae7b68bea695cd5a0d16e42
murrayj1 <murrayj1@rose-hulman.edu>
2019-03-04 08:34:08 -0500

    Done

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -16,7 +16,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   (Yes, that means for YOU to DO things per these instructions:)
 #
 #   Run this module by right clicking anywhere in this window
@@ -32,7 +32,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a thing TO-DO in this module.
 #
@@ -57,9 +57,10 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 3.
+# Done: 3.
 #   Add another   print   statement below this comment.
 #   It should print any string that you want (but keep it polite and G-rated!)
+print('Will I like MATLAB more?')
 #
 #   Test your code by re-running this module, either by proceeding
 #   as you did when you ran this module the first time,
@@ -71,11 +72,12 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 4.
+# Done: 4.
 #   Add yet another   print   statement, putting it below this comment.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
 #   You do NOT have to use strings for this, so no quotation marks!
+print(3607*34227)
 #
 #   TEST your code by re-running this module, then asking someone
 #   whom you trust:
